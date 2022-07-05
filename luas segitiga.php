4acf98ef7c6b2bcabb4b51ad1ebf31059d601b76
Bismantara <101230628+Bismantara@users.noreply.github.com>
2022-07-05 19:21:30 +0800

    wp_project1

---
+++ luas segitiga.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Menghitung Luas Segitiga Form</title>
+</head>
+<body>
+    <h1> Hitung Luas Segitiga </h1> <br>
+    <form method="POST" action="#">
+        Nilai Alas:<br>
+        <input type="number" name="txALAS" min="0" max="999" required><br><br>
+
+        <form method="POST" action="#">
+            Nilai Tinggi:<br>
+            <input type="number" name="txTINGGI" min="0" max="999" required><br><br>  
+            <textarea name="luas" id="luas">
+        <?php
+            
+        if(isset($_POST['hasil'])){
+                $alas    =$_POST["txALAS"];
+                $tinggi    =$_POST["txTINGGI"];
+                $luas = ($alas * $tinggi) /2 ;
+                echo $luas ;
+            }
+            ?>
+            </textarea>
+            <br> <input type="submit" name="hasil" value="hasil">
+            
+        </form>
+      
+        
+        
+    </form>
+</body>
+</html>
